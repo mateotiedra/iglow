470fb296031c340bcb2d6b748cd8cc396d38cf75
mateotiedra <mateo.tiedra@outlook.com>
2024-09-13 23:58:23 +0200

    update faq

--- src/components/FAQSection/FAQSectionLogic.js
+++ src/components/FAQSection/FAQSectionLogic.js
@@ -3,24 +3,42 @@ import { useState } from 'react';
 const FAQSectionLogic = () => {
   const questions = [
     {
-      title: 'Does it light up ?',
+      title:
+        'When and how will I receive my IGLOW smart jacket after supporting the campaign?',
       details:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+        'After the campaign ends, we will maintain complete transparency regarding the production process and updates to ensure all contributors are satisfied. One of our key advantages is that production will be done locally, with both the electronics and textiles manufactured in Switzerland. This proximity allows us to maintain high quality and efficiency, ensuring you receive your IGLOW vest as soon as possible after the end of the campaign.',
     },
     {
-      title: 'How long does the battery last ?',
+      title: 'What inspired the creation of the IGLOW smart jacket?',
       details:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+        'IGLOW was born from the personal experience of its creator, who, after purchasing a scooter to commute to work, realized how dangerous it was not to be visible on the road. With their expertise in textiles and fashion, having worked with luxury brands, the creators of IGLOW developed an innovative and stylish safety solution.',
     },
     {
-      title: 'Where can I buy it ?',
+      title:
+        'How does IGLOW improve the safety of cyclists and users of various types of bikes?',
       details:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+        'The IGLOW smart jacket is designed to make users of all types of bikes, whether cargo bikes, powerful e-bikes or conventional bikes, highly visible thanks to its integrated LEDs, reflective strips and Bluetooth remote control. Drawing on their experience in the fashion industry, the designers have ensured that every aspect of the jacket combines functionality, comfort, lightness and aesthetics for maximum safety, day and night.',
     },
     {
-      title: 'Who is it for ?',
+      title: 'What are the features and benefits of the IGLOW smart jacket?',
       details:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
+        'IGLOW is a lightweight and comfortable safety vest made with high-quality materials. It comes equipped with numerous features, including flashing LEDs, a rechargeable battery with over 48 hours of autonomy, practical pockets, and an emergency card information window. The creators, who have experience in the luxury industry, also ensured that the vest can be worn over a backpack, offering both style and safety.',
+    },
+    {
+      title: 'How is IGLOW different from other safety smart jacket ?',
+      details:
+        "In addition to being equipped with brake lights and turn signals for cyclists, the IGLOW vest is versatile and can be used in various situations. It's perfect for evening or early morning runners  to ensure you're seen, and it can also be used in case of a roadside breakdown to signal your presence with its built-in warning lights. The IGLOW smart jacket is unique and designed to accompany you every day, providing both safety and style wherever you go.",
+    },
+    {
+      title: 'Is IGLOW compatible with all types of bikes and scooters?',
+      details:
+        'Yes, the IGLOW vest is designed to be compatible with all types of bikes, including cargo bikes, powerful electric bikes, and scooters. Its Bluetooth remote control easily attaches to the handlebar, and the vest can be comfortably worn with or without a backpack.',
+    },
+    {
+      title:
+        'Why is the IGLOW smart jacket so affordable given that it’s made in Switzerland?',
+      details:
+        'The affordable price of the IGLOW smart jacket is an exceptional offer exclusively for Indiegogo contributors. Normally, the price is higher, but we have deliberately set this attractive price to allow as many people as possible to benefit from using IGLOW. Our goal is to make the IGLOW smart jacket a global reference in safety. By producing locally in Switzerland, we ensure top-notch quality, combining advanced technology, safety, and style at a price point that’s truly a rare opportunity.',
     },
   ];
 
